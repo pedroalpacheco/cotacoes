1da90b9bafc0db488aebc869dcdfdd1690185b5f
papacheco <pedro.pacheco.a@gmail.com>
2017-12-08 08:11:20 -0200

    Alterado view de consulta pata ID

--- moedas/core/views.py
+++ moedas/core/views.py
@@ -3,5 +3,5 @@ from .models import Dolar
 
 def home(request):
     #dolares = Dolar.objects.values().order_by('-data')[:1]
-    dolares = Dolar.objects.values().order_by('-data')[0:1].get()
+    dolares = Dolar.objects.values().order_by('-id')[0:1].get()
     return render(request, 'index.html', {'dolares':dolares})
